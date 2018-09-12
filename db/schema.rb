82e22ccd2fce30a3274ef180a8bf3e9edd2f2c8f
Bharat <bharat.soni@bacancytechnology.com>
2018-09-12 10:45:07 +0530

    create project scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_09_12_050713) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.date "launch_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
