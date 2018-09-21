738f355afb255d52bf039435e92da5bbf9e5d753
bacancy-namrata <namrata.soni@bacancytechnology.com>
2018-09-21 12:44:49 +0530

    add model for comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_12_051644) do
+ActiveRecord::Schema.define(version: 2018_09_21_071253) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.integer "task_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_comments_on_task_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
